f8103749ea75fb530366ce78899414c42a944b93
Carlos Akion Garro Campos <carloscamp1008@estudiantec.cr>
2021-06-08 23:46:03 -0600

    DP

--- Extraclase_III/DeteccionParidad.cpp
+++ Extraclase_III/DeteccionParidad.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 // Function to find the parity
+//    El bit de "y" localizado más a la derecha contiene el valor de paridad
+//    si (y & 1) es 1, entonces la paridad es impar si no es par
 bool DeteccionParidad::findParity(int x)
 {
     int y = x ^ (x >> 1);
@@ -11,8 +13,6 @@ bool DeteccionParidad::findParity(int x)
     y = y ^ (y >> 8);
     y = y ^ (y >> 16);
 
-    // Rightmost bit of y holds the parity value
-    // if (y&1) is 1 then parity is odd else even
     if (y & 1)
         return 1;
     return 0;
